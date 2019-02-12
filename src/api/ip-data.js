b4249a2b36edcb1799b6fa7c5e65963100e2d0e3
slorenzo <SebastianLorenzo@gmail.com>
2019-02-12 20:18:19 -0300

    Added microbundle && parcel-bundler

--- src/api/ip-data.js
+++ src/api/ip-data.js
@@ -1,7 +1,7 @@
-import * as urljoin from 'url-join'
+import urlJoin from 'url-join'
 
 const ipdata = (ip, apiKey) => {
-  const url = urljoin('https://api.ipdata.co/', `?api-key=${apiKey}`)
+  const url = urlJoin('https://api.ipdata.co/', `?api-key=${apiKey}`)
 
   return fetch(url, { method: 'GET' }) // eslint-disable-line
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -5,7 +5,7 @@ import Adapter from 'enzyme-adapter-react-16'
 
 import fetchMock from 'fetch-mock'
 
-import ReactLocalCurrency from 'src/'
+import LocalCurrency from '../src'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -13,14 +13,14 @@ const noop = () => undefined
 const wait = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms))
 
 describe('React Local Currency', () => {
-  before(() => {
+  beforeAll(() => {
     fetchMock.mock('https://openexchangerates.org/api/latest.json?base=USD&app_id=test', require('./get-rates.mock.json'))
     fetchMock.mock('https://api.ipdata.co/?api-key=test', require('./ip-data.mock.json'))
   })
 
   it('It renders without breaking using render as property', () => {
     const wrapper = shallow(
-      <ReactLocalCurrency
+      <LocalCurrency
         amount={7}
         base='USD'
         IPDATA_API_KEY='test'
@@ -34,14 +34,14 @@ describe('React Local Currency', () => {
 
   it('It renders without breaking using render as children elements', () => {
     const wrapper = shallow(
-      <ReactLocalCurrency
+      <LocalCurrency
         amount={7}
         base='USD'
         IPDATA_API_KEY='test'
         OXR_API_ID='test'
       >
         {() => <div />}
-      </ReactLocalCurrency>
+      </LocalCurrency>
     )
 
     expect(wrapper.length).toBe(1)
@@ -49,7 +49,7 @@ describe('React Local Currency', () => {
 
   it('It renders breaking - Wrong initialize ', () => {
     const wrapper = shallow(
-      <ReactLocalCurrency
+      <LocalCurrency
         amount={7}
         base='USD'
         IPDATA_API_KEY='test'
@@ -62,7 +62,7 @@ describe('React Local Currency', () => {
 
   it('Initialize sets initial state', async () => {
     const wrapper = shallow(
-      <ReactLocalCurrency
+      <LocalCurrency
         amount={7}
         base='USD'
         IPDATA_API_KEY='test'
@@ -78,7 +78,7 @@ describe('React Local Currency', () => {
 
   it('It should convert USD to ARS', async () => {
     const wrapper = shallow(
-      <ReactLocalCurrency
+      <LocalCurrency
         amount={7}
         base='USD'
         IPDATA_API_KEY='test'
